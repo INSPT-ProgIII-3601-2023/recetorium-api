7bd1eef01c37aa576eb2c6bef05343cd6d531737
Charly <carlosecimino@gmail.com>
2023-09-26 12:23:11 -0300

    CRUD de ingredientes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
+require('dotenv').config();
+
+const Server = require('./Server');
+
+const server = new Server();
+
+server.listen();
---
+++ routes/ingredientes.js
@@ -0,0 +1,15 @@
+// https://expressjs.com/en/guide/routing.html
+const express = require('express');
+
+const {getTodos, getByID, agregar, editar, borrar} = require('../controllers/ingredientesDBFake')
+
+const router = express.Router();
+
+router.get('/', getTodos);
+router.get('/:id', getByID);
+router.post('/', agregar);
+router.put('/:id', editar);
+router.delete('/:id', borrar);
+// Faltan otros endpoints...
+
+module.exports = router;
